c3f91a3cf13e0bc6f879136213e84406805245a0
cljzxx <9898910@qq.com>
2025-04-09 20:13:55 +0800

    元数据相关、自定义练习
    增加test频道并尝试tailwindcss

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Test', href: '/dashboard/test', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
@@ -35,7 +36,7 @@ export default function NavLinks() {
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
           >
             <LinkIcon className="w-6" />
